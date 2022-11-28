6a82a5e7647f2e4bbe01f82f47dad9c174b60c89
Christoph Moarales <christoph.julian.mayerhofer@gmail.com>
2022-11-28 21:02:31 +0100

    now using accelometor

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -4,8 +4,14 @@ func _process(delta):
 	gravity_scale = 0
 	var prevVelocity = linear_velocity;
 	var gyroVector = Input.get_gyroscope()
+	
+	var accVector = Input.get_accelerometer();
+	var accDirection = Vector2(accVector.x,-accVector.y);
 	var direction =  Vector2(gyroVector.y,gyroVector.x);
-	add_force(direction,direction*500)
+	
+	var bulletDirection = accDirection;
+	
+	add_force(bulletDirection,bulletDirection*100)
 	if prevVelocity.length() > 0  :
 	 linear_velocity = prevVelocity;
 	
